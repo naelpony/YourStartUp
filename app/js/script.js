efae0a1c8a53c98ad29b142f74cbe481cfb9c26d
naelpony <naelpony@gmail.com>
2021-09-13 02:49:38 +0300

    end commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,15 @@
+
 $(function () {
 	$("#phone").mask("8(999) 999 99 99");
 });
 $(function () {
 	$("#phonef").mask("8(999) 999 99 99");
 });
+
+
+
+$('.menu__burger').on('click', function(event){
+	$('.menu__burger, .menu-center').toggleClass('active');
+	$('body').toggleClass('lock')
+});
+
